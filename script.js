806c1a3bb34c767ba6b85913fd06c09dcf4b9078
artem <artemfr@hotmail.com>
2024-06-02 13:17:46 +0200

    Added api url and variables in js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiURL = 'https://rickandmortyapi.com/api/character';
+    const characterList = document.getElementById('characterList');
+    const characterDetail = document.getElementById('characterDetail');
+    const searchBar = document.getElementById('searchBar');
+    const filterButtons = document.querySelectorAll('.filter-button');
+    const firstPageButton = document.getElementById('firstPage');
+    const lastPageButton = document.getElementById('lastPage');
+    const pageNumbersContainer = document.getElementById('pageNumbers');
+    let characters = [];
+    let currentPage = 1;
+    let totalPages = 1;
+    let currentFilter = 'all';
+    let searchQuery = '';
